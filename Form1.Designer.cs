a207f4dc8692befa88d3a9cacc891a2fb06dfead
Fhwang <hdh0926@naver.com>
2020-04-06 00:33:45 +0900

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿using System;
+
+namespace CoreTracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.l_core = new System.Windows.Forms.Label();
+            this.l_th = new System.Windows.Forms.Label();
+            this.l_th_value = new System.Windows.Forms.Label();
+            this.l_core_value = new System.Windows.Forms.Label();
+            this.btn_tray = new System.Windows.Forms.Button();
+            this.ti_main = new System.Windows.Forms.NotifyIcon(this.components);
+            this.ti_menu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.SuspendLayout();
+            // 
+            // l_core
+            // 
+            this.l_core.AutoSize = true;
+            this.l_core.Font = new System.Drawing.Font("굴림", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.l_core.Location = new System.Drawing.Point(12, 35);
+            this.l_core.Name = "l_core";
+            this.l_core.Size = new System.Drawing.Size(206, 38);
+            this.l_core.TabIndex = 0;
+            this.l_core.Text = "Core      : ";
+            // 
+            // l_th
+            // 
+            this.l_th.AutoSize = true;
+            this.l_th.Font = new System.Drawing.Font("굴림", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.l_th.Location = new System.Drawing.Point(12, 96);
+            this.l_th.Name = "l_th";
+            this.l_th.Size = new System.Drawing.Size(205, 38);
+            this.l_th.TabIndex = 1;
+            this.l_th.Text = "Thread   : ";
+            // 
+            // l_th_value
+            // 
+            this.l_th_value.AutoSize = true;
+            this.l_th_value.Font = new System.Drawing.Font("굴림", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.l_th_value.Location = new System.Drawing.Point(211, 96);
+            this.l_th_value.Name = "l_th_value";
+            this.l_th_value.Size = new System.Drawing.Size(39, 38);
+            this.l_th_value.TabIndex = 3;
+            this.l_th_value.Text = "0";
+            // 
+            // l_core_value
+            // 
+            this.l_core_value.AutoSize = true;
+            this.l_core_value.Font = new System.Drawing.Font("굴림", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.l_core_value.Location = new System.Drawing.Point(211, 35);
+            this.l_core_value.Name = "l_core_value";
+            this.l_core_value.Size = new System.Drawing.Size(39, 38);
+            this.l_core_value.TabIndex = 2;
+            this.l_core_value.Text = "0";
+            // 
+            // btn_tray
+            // 
+            this.btn_tray.Location = new System.Drawing.Point(19, 151);
+            this.btn_tray.Name = "btn_tray";
+            this.btn_tray.Size = new System.Drawing.Size(337, 44);
+            this.btn_tray.TabIndex = 4;
+            this.btn_tray.Text = "Toggle Tray";
+            this.btn_tray.UseVisualStyleBackColor = true;
+            this.btn_tray.Click += new System.EventHandler(this.btn_tray_Click);
+            // 
+            // ti_main
+            // 
+            this.ti_main.Icon = ((System.Drawing.Icon)(resources.GetObject("ti_main.Icon")));
+            this.ti_main.Text = "notifyIcon1";
+            this.ti_main.Visible = true;
+            this.ti_main.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // ti_menu
+            // 
+            this.ti_menu.Name = "ti_menu";
+            this.ti_menu.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 210);
+            this.Controls.Add(this.btn_tray);
+            this.Controls.Add(this.l_th_value);
+            this.Controls.Add(this.l_core_value);
+            this.Controls.Add(this.l_th);
+            this.Controls.Add(this.l_core);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "CoreTracker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label l_core;
+        private System.Windows.Forms.Label l_th;
+        private System.Windows.Forms.Label l_th_value;
+        private System.Windows.Forms.Label l_core_value;
+        private System.Windows.Forms.Button btn_tray;
+        private System.Windows.Forms.NotifyIcon ti_main;
+        private System.Windows.Forms.ContextMenuStrip ti_menu;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoreTracker
+{
+    public partial class Form1 : Form
+    {
+        private List<NotifyIcon> th_list = new List<NotifyIcon>();
+        private bool run = false;
+        private Thread th;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // under code run after InitializeComponent
+            l_core_value.Text = (Environment.ProcessorCount / 2).ToString();
+            l_th_value.Text = Environment.ProcessorCount.ToString();
+
+            ti_main.Visible = true;
+            ti_main.ContextMenu = new ContextMenu();
+            ti_main.ContextMenu.MenuItems.Add(0, new MenuItem("Show", new System.EventHandler(Show_Click)));
+            ti_main.ContextMenu.MenuItems.Add(1, new MenuItem("Hide", new System.EventHandler(Hide_Click)));
+            ti_main.ContextMenu.MenuItems.Add(2, new MenuItem("Exit", new System.EventHandler(Exit_Click)));
+
+            for (int c = 1; c <= Environment.ProcessorCount; c++)
+            {
+                th_list.Add(new NotifyIcon() { Visible = true, Icon = Properties.Resources._10 });
+            }
+            // intialize thread
+            th = new Thread(new ThreadStart(runner));
+
+        }
+        protected void Exit_Click(Object sender, System.EventArgs e)
+        {
+            Close();
+        }
+        protected void Hide_Click(Object sender, System.EventArgs e)
+        {
+            Hide();
+        }
+        protected void Show_Click(Object sender, System.EventArgs e)
+        {
+            Show();
+        }
+
+        private void btn_tray_Click(object sender, EventArgs e)
+        {
+            Hide();
+            ti_main.Visible = true;
+
+            if (!run) { run = true; th.Start(); }
+        }
+
+        private void runner()
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("select * from Win32_PerfFormattedData_PerfOS_Processor");
+            while (true)
+            {
+                System.Threading.Thread.Sleep(3000);
+                var cpu_info = searcher.Get().Cast<ManagementObject>().Select(mo => new { Name = mo["Name"], Usage = Convert.ToInt32(mo["PercentProcessorTime"]) }).ToList();
+                foreach (var c in cpu_info)
+                {
+                    if (c.Name.ToString() == "_Total")
+                    {
+                        //l_cpu_value.Text = c.Usage.ToString();
+                    }
+                    else
+                    {
+                        th_list[Convert.ToInt32(c.Name)].Icon = setTrayIcon(c.Usage);
+                    }
+                }
+            }
+        }
+
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            ti_main.Visible = false;
+            
+        }
+
+        private dynamic setTrayIcon(int value)
+        {
+            if (0 <= value && value <= 10) { return Properties.Resources._10; }
+            else if (10 < value && value <= 20) { return Properties.Resources._20; }
+            else if (20 < value && value <= 40) { return Properties.Resources._40; }
+            else if (40 < value && value <= 60) { return Properties.Resources._60; }
+            else { return Properties.Resources._80; }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Resize(object sender, System.EventArgs e)
+        {
+            // Hide The Form when it's minimized
+            if (FormWindowState.Minimized == WindowState) { 
+                Hide();
+                ti_main.Visible = true;
+                if (!run) { run = true; th.Start(); }
+            }
+                
+        }
+
+    }
+}
